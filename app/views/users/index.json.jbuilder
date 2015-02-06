ab469bd27807c06763684f063657d6e4903aa592
Prav92 <cbkmar92@gmail.com>
2015-02-06 06:10:00 +0000

    extra field added

--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@users) do |user|
-  json.extract! user, :id, :name, :email, :address, :phone
+  json.extract! user, :id, :name, :email, :address, :country, :phone
   json.url user_url(user, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204070752) do
+ActiveRecord::Schema.define(version: 20150206055526) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20150204070752) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "country"
+    t.string   "string"
   end
 
 end
